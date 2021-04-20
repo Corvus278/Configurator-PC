1cd82bf09f0a4ee62817690a06bf602cd5fa5434
Corvus <makar.korolenko2005@mail.ru>
2021-04-20 14:31:01 +0300

    Парсер находит ссылку на характеристики каждого продукта

--- py/Парсеры/parser.py
+++ py/Парсеры/parser.py
@@ -1,20 +1,42 @@
 from selenium import webdriver
+import time
 
 
 def main():
+    # Запускаем браузер
+    start1 = time.time()
+    start_link = 'https://www.e-katalog.ru/ek-list.php?katalog_=187&sb_=материнская+плата'
     driver = webdriver.Chrome()
-    driver.get('https://www.e-katalog.ru/ek-list.php?katalog_=187&sb_=материнская+плата')
+    driver.get(start_link)
 
+    start = time.time()
+    # Вытаскиваем все ссылки из страницы
     all_links = driver.find_elements_by_tag_name('a')
-
-    boards_links_url = []
+    # Из всех ссылок берём те, которые ведут на продукт, и добавляем в список
+    boards_links_urls = []
     for link in all_links:
         if link.get_attribute('data-text') == 'ВЫ ИСКАЛИ':
             url = link.get_attribute('href')
-            boards_links_url.append(url)
+            boards_links_urls.append(url)
 
-    print(boards_links_url)
+    # Переходим к продукту, и берём от туда ссылку на характеристики, добавляем в список
+    specifications_link_urls = []
+    for board_link in boards_links_urls:
+        driver.get(board_link)
+        all_links = driver.find_elements_by_tag_name('a')
+        for link in all_links:
+            if link.text == 'ХАРАКТЕРИСТИКИ':
+                url = 'https://www.e-katalog.ru' + link.get_attribute('link')
+                specifications_link_urls.append(url)
+                break
+
+
+
+    end = time.time()
+    print(end - start)
+    print(end - start1)
 
 
 if __name__ == '__main__':
     main()
+
