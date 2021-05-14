1a235bf7512543260d123a48c5df5fafe0073546
Corvus <makar.korolenko2005@mail.ru>
2021-05-14 12:38:52 +0300

    Поместил все данные в базу данных

--- py/db/filler.py
+++ py/db/filler.py
@@ -2,10 +2,15 @@ import sqlite3
 import parts_list as pl
 
 
-def database_start():
+def sql_push(table_name, parts, cur):
+    for part in parts:
+        request = "INSERT INTO" + ' ' + table_name + "(dict) VALUES(?)"
+        cur.execute(request, (part,))
+
+
+def main():
     # Подключаем бд
     conn = sqlite3.connect('parts.db')
-
     # Содаём курсор для осуществления SQL запросов
     cur = conn.cursor()
 
@@ -15,29 +20,27 @@ def database_start():
     cur.execute("CREATE TABLE IF NOT EXISTS gpu(dict TEXT)")
     cur.execute("CREATE TABLE IF NOT EXISTS ram(dict TEXT)")
     cur.execute("CREATE TABLE IF NOT EXISTS power_supply(dict TEXT)")
-    cur.execute("CREATE TABLE IF NOT EXISTS case(dict TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS case_(dict TEXT)")
     cur.execute("CREATE TABLE IF NOT EXISTS storage(dict TEXT)")
     cur.execute("CREATE TABLE IF NOT EXISTS cooler(dict TEXT)")
     cur.execute("CREATE TABLE IF NOT EXISTS wcs(dict TEXT)")
     cur.execute("CREATE TABLE IF NOT EXISTS fan(dict TEXT)")
 
+    # Добавляем данные в базу
+    sql_push('motherboard', pl.motherboards, cur)
+    sql_push('case_', pl.cases, cur)
+    sql_push('cpu', pl.cpu, cur)
+    sql_push('gpu', pl.gpu, cur)
+    sql_push('ram', pl.ram, cur)
+    sql_push('power_supply', pl.power_supplies, cur)
+    sql_push('storage', pl.storage, cur)
+    sql_push('cooler', pl.coolers, cur)
+    sql_push('fan', pl.fans, cur)
 
-def sql_push(table_name, parts):
-    for part in parts:
-        cur.execute(f"INSERT INTO {table_name}(dict) VALUES({part})")
-
-
-def main():
-    database_start()
-    sql_push('motherboard', pl.motherboards)
-    sql_push('cpu', pl.cpu)
-    sql_push('gpu', pl.gpu)
-    sql_push('ram', pl.ram)
-    sql_push('power_supply', pl.power_supply)
-    sql_push('storage', pl.storage)
-    sql_push('cooler', pl.coolers)
-    sql_push('fan', pl.fans)
+    # Коммитим и закрываем бд
+    conn.commit()
+    cur.close()
 
 
-if __name__ == '__main':
+if __name__ == '__main__':
     main()
